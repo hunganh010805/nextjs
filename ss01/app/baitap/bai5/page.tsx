b9e72345c7d8eeae13ee134801a6d019d08e9a91
hunganh010805 <teugiatre123@gmail.com>
2024-08-21 15:02:28 +0700

    update

---
+++ ss01/app/baitap/bai5/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type props = {
+  btn:string[]
+}
+
+import default function Button(props: Props){
+  // console.log("props",props);
+  return{
+    <div>
+    {props.btn.map((item,index))
+    }
+    </div>
+  }
+}
+
